3ae9c3a684fc8a17d2438039f81469ed8daa8faf
Ollie Jones <oathompsonjones@gmail.com>
2021-03-13 21:52:38 +0000

    Update home.php

--- Pages/student/home.php
+++ Pages/student/home.php
@@ -9,5 +9,7 @@ HTML;
 $animations = unserialize($_SESSION["user"])->animations;
 $animation = new MicroBitBuiltInAnimation($animations[0]->id);
 $json = $animation->getFramesAs32BitIntegersJSON();
-echo "<h3>" . $animation->name . "</h3><br><code><pre>"
-    . $animation->generateMicroPythonCode($json) . "</pre></code>";
+echo "<h3>" . $animation->name . " - " . "MicroPython</h3><br><code><pre>"
+    . $animation->generateMicroPythonCode($json) . "</pre></code>" . "<br>"
+    . "<h3>" . $animation->name . " - " . "TypeScript</h3><br><code><pre>"
+    . $animation->generateTypeScriptCode($json) . "</pre></code>";
