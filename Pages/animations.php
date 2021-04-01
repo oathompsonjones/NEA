ea4c4aac3d34221418f56ac984c8daa87489e85a
Ollie Jones <oathompsonjones@gmail.com>
2021-04-02 00:03:37 +0100

    Update animations.php

--- Pages/animations.php
+++ Pages/animations.php
@@ -1,16 +1,12 @@
 <?php
 $db = $_SESSION["database"];
 $user = unserialize($_SESSION["user"]);
-
 echo <<<HTML
     <h1>My Animations</h1>
     <div class="row row-cols-1 row-cols-md-5 g-4">
         <style>.animation { display: none; }</style>
 HTML;
 $animations = $user->animations;
-$posts = $user->posts;
-$postedAnimationIDs = array_map("mapToAnimationID", $posts);
-
 for ($i = 0; $i < count($animations); ++$i) {
     $html = $animations[$i]->render();
     echo <<<HTML
