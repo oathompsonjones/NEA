393a4cdb10586a4ac143a209d508d1d795e7c820
Ollie Jones <oathompsonjones@gmail.com>
2021-04-28 11:50:54 +0100

    Update home.php

--- Pages/home.php
+++ Pages/home.php
@@ -1,12 +1,20 @@
 <?php
+// Get the required session variables.
 $db = $_SESSION["database"];
 $user = unserialize($_SESSION["user"]);
 
+/**
+ * Renders all Users from the database.
+ * @return string
+ */
 function renderUsers()
 {
+    // Get the required session variables.
     $db = $_SESSION["database"];
     $user = unserialize($_SESSION["user"]);
+    // Get an object for each user.
     $users = array_map("mapToUserObject", array_map("mapToFirstItem", $db->select("Username", "User", NULL, "Username")));
+    // Create the table and headings to display each user.
     $html = <<<HTML
         <h3>Users</h3>
         <table style="width: 100%;">
@@ -23,8 +31,11 @@ function renderUsers()
                 <th></th>
             </tr>
     HTML;
+    // Count the users so that it doesn't need recalculating on each iteration.
     $userCount = count($users);
+    // Loop through each user.
     for ($i = 0; $i < $userCount; ++$i) {
+        // Render the basic information.
         $username = $users[$i]->username;
         $passwordHash = $users[$i]->passwordHash;
         $resetPasswordHash = md5($username);
@@ -35,6 +46,7 @@ function renderUsers()
         $following = count($users[$i]->following);
         $html .= <<<HTML
             <tr id="$username-row">
+                <!-- Links to the user's profile. -->
                 <td><a href="profile?user=$username">$username</a></td>
                 <td id="$username-password">$passwordHash</td>
                 <td id="$username-type">$type</td>
@@ -43,6 +55,7 @@ function renderUsers()
                 <td>$followers</td>
                 <td>$following</td>
         HTML;
+        // Do not allow an admin to delete themselves.
         if ($username === $user->username) {
             $html .= <<<HTML
                 <td>
@@ -50,9 +63,12 @@ function renderUsers()
                 </td>
             HTML;
         } else {
+            // Creates a button to delete the user.
             $html .= <<<HTML
                 <script>
+                    // Changes the delete button to a confirm button.
                     const delete_$username = () => document.getElementById("$username-delete").innerHTML = `<button onclick="deleteConfirm_$username();" class="btn btn-sm btn-danger">Confirm</button>`;
+                    // Runs an AJAX request to delete the user.
                     const deleteConfirm_$username = () => {
                         const username = "$username";
                         $.post("Utils/Forms/deleteUser.php", { username }, () => {
@@ -61,14 +77,18 @@ function renderUsers()
                         });
                     };
                 </script>
+                <!-- Adds the delete button to the table. -->
                 <td id="$username-delete">
                     <button onclick="delete_$username();" class="btn btn-sm btn-danger">Delete</button>
                 </td>
             HTML;
         }
+        // Reset password button.
         $html .= <<<HTML
             <script>
+                // Changes the reset button to a confirm button.
                 const resetPassword_$username = () => document.getElementById("$username-resetPassword").innerHTML = `<button onclick="resetPasswordConfirm_$username();" class="btn btn-sm btn-warning">Confirm</button>`;
+                // Runs an AJAX request to reset the password.
                 const resetPasswordConfirm_$username = () => {
                     const username = "$username";
                     $.post("Utils/Forms/resetPassword.php", { username }, () => {
@@ -77,10 +97,12 @@ function renderUsers()
                     });
                 };
             </script>
+            <!-- Adds the reset button to the table. -->
             <td id="$username-resetPassword">
                 <button onclick="resetPassword_$username();" class="btn btn-sm btn-warning">Reset Password</button>
             </td>
         HTML;
+        // Can't make an admin into an admin.
         if ($type === "admin") {
             $html .= <<<HTML
                 <td>
@@ -88,9 +110,12 @@ function renderUsers()
                 </td>
             HTML;
         } else {
+            // Make admin button.
             $html .= <<<HTML
                 <script>
+                    // Changes the make admin button to a confirm button.
                     const makeAdmin_$username = () => document.getElementById("$username-makeAdmin").innerHTML = `<button onclick="makeAdminConfirm_$username();" class="btn btn-sm btn-dark">Confirm</button>`;
+                    // Runs an AJAX request to make the user an admin.
                     const makeAdminConfirm_$username = () => {
                         const username = "$username";
                         $.post("Utils/Forms/makeAdmin.php", { username }, () => {
@@ -99,6 +124,7 @@ function renderUsers()
                         });
                     };
                 </script>
+                <!-- Adds the admin button to the table. -->
                 <td id="$username-makeAdmin">
                     <button onclick="makeAdmin_$username();" class="btn btn-sm btn-dark">Make Admin</button>
                 </td>
@@ -114,10 +140,17 @@ function renderUsers()
     return $html;
 }
 
+/**
+ * Renders all Animations from the database.
+ * @return string
+ */
 function renderAnimations()
 {
+    // Get the required session variables.
     $db = $_SESSION["database"];
+    // Get an object for each animation.
     $animations = array_map("mapToAnimationObject", array_map("mapToFirstItem", $db->select("AnimationID", "Animation", NULL, "AnimationID")));
+    // Create the table and headings to display each animation.
     $html = <<<HTML
         <h3>Animations</h3>
         <table style="width: 100%;">
@@ -132,8 +165,11 @@ function renderAnimations()
                 <th></th>
             </tr>
     HTML;
+    // Count the animations so that it doesn't need recalculating on each iteration.
     $animationCount = count($animations);
+    // Loop through each animation.
     for ($i = 0; $i < $animationCount; ++$i) {
+        // Render the basic information.
         $id = $animations[$i]->id;
         $name = $animations[$i]->name;
         $width = $animations[$i]->width;
@@ -149,9 +185,12 @@ function renderAnimations()
                 <td>$height</td>
                 <td>$type</td>
                 <td>$frames</td>
+                <!-- Links to the user's profile. -->
                 <td><a href="profile?user=$user->username">$user->username</a></td>
                 <script>
+                    // Changes the delete button to a confirm button.
                     const delete_$id = () => document.getElementById("$id-delete").innerHTML = `<button onclick="deleteConfirm_$id();" class="btn btn-sm btn-danger">Confirm</button>`;
+                    // Runs an AJAX request to delete the animation.
                     const deleteConfirm_$id = () => {
                         const animationID = "$id";
                         $.post("Utils/Forms/deleteAnimation.php", { animationID }, () => {
@@ -160,6 +199,7 @@ function renderAnimations()
                         });
                     };
                 </script>
+                <!-- Delete button. -->
                 <td id="$id-delete">
                     <button onclick="delete_$id();" class="btn btn-sm btn-danger">Delete</button>
                 </td>
@@ -172,10 +212,17 @@ function renderAnimations()
     return $html;
 }
 
+/**
+ * Renders all Posts from the database.
+ * @return string
+ */
 function renderPosts()
 {
+    // Get the required session variables.
     $db = $_SESSION["database"];
+    // Get an object for each post.
     $posts = array_map("mapToPostObject", array_map("mapToFirstItem", $db->select("PostID", "Post", NULL, "PostID")));
+    // Create the table and headings to display each post.
     $html = <<<HTML
         <h3>Posts</h3>
         <table style="width: 100%;">
@@ -190,7 +237,9 @@ function renderPosts()
                 <th></th>
             </tr>
     HTML;
+    // Count the posts so that it doesn't need recalculating on each iteration.
     $postCount = count($posts);
+    // Loop through each post.
     for ($i = 0; $i < $postCount; ++$i) {
         $id = $posts[$i]->id;
         $animation = $posts[$i]->animationID;
@@ -207,9 +256,12 @@ function renderPosts()
                 <td>$comments</td>
                 <td>$likes</td>
                 <td>$createdAt</td>
+                <!-- Links to the user's profile. -->
                 <td><a href="profile?user=$user->username">$user->username</a></td>
                 <script>
+                    // Changes the delete button to a confirm button.
                     const delete_$id = () => document.getElementById("$id-delete").innerHTML = `<button onclick="deleteConfirm_$id();" class="btn btn-sm btn-danger">Confirm</button>`;
+                    // Runs an AJAX request to delete the post.
                     const deleteConfirm_$id = () => {
                         const postID = "$id";
                         $.post("Utils/Forms/unShareAnimation.php", { postID }, () => {
@@ -218,6 +270,7 @@ function renderPosts()
                         });
                     };
                 </script>
+                <!-- Delete button. -->
                 <td id="$id-delete">
                     <button onclick="delete_$id();" class="btn btn-sm btn-danger">Delete</button>
                 </td>
@@ -230,10 +283,17 @@ function renderPosts()
     return $html;
 }
 
+/**
+ * Renders all Comments from the database.
+ * @return string
+ */
 function renderComments()
 {
+    // Get the required session variables.
     $db = $_SESSION["database"];
+    // Get an object for each comment.
     $comments = array_map("mapToCommentObject", array_map("mapToFirstItem", $db->select("CommentID", "Comment", NULL, "CommentID")));
+    // Create the table and headings to display each comment.
     $html = <<<HTML
         <h3>Comments</h3>
         <table style="width: 100%;">
@@ -246,7 +306,9 @@ function renderComments()
                 <th></th>
             </tr>
     HTML;
+    // Count the posts so that it doesn't need recalculating on each iteration.
     $commentCount = count($comments);
+    // Loop through each comment.
     for ($i = 0; $i < $commentCount; ++$i) {
         $id = $comments[$i]->id;
         $content = $comments[$i]->content;
@@ -259,9 +321,12 @@ function renderComments()
                 <td style="max-width: 500px; word-wrap: break-word;">$content</td>
                 <td>$post</td>
                 <td>$createdAt</td>
+                <!-- Links to the user's profile. -->
                 <td><a href="profile?user=$user->username">$user->username</a></td>
                 <script>
+                    // Changes the delete button to a confirm button.
                     const delete_$id = () => document.getElementById("$id-delete").innerHTML = `<button onclick="deleteConfirm_$id();" class="btn btn-sm btn-danger">Confirm</button>`;
+                    // Runs an AJAX request to delete the comment.
                     const deleteConfirm_$id = () => {
                         const commentID = "$id";
                         $.post("Utils/Forms/deleteComment.php", { commentID }, () => {
@@ -270,6 +335,7 @@ function renderComments()
                         });
                     };
                 </script>
+                <!-- Delete button. -->
                 <td id="$id-delete">
                     <button onclick="delete_$id();" class="btn btn-sm btn-danger">Delete</button>
                 </td>
@@ -282,10 +348,17 @@ function renderComments()
     return $html;
 }
 
+/**
+ * Renders all Classes from the database.
+ * @return string
+ */
 function renderClasses()
 {
+    // Get the required session variables.
     $db = $_SESSION["database"];
+    // Get an object for each class.
     $classes = array_map("mapToGroupObject", array_map("mapToFirstItem", $db->select("ClassID", "Class", NULL, "ClassID")));
+    // Create the table and headings to display each class.
     $html = <<<HTML
         <h3>Classes</h3>
         <table style="width: 100%;">
@@ -301,7 +374,9 @@ function renderClasses()
                 <th></th>
             </tr>
     HTML;
+    // Count the posts so that it doesn't need recalculating on each iteration.
     $classCount = count($classes);
+    // Loop through each class.
     for ($i = 0; $i < $classCount; ++$i) {
         $id = $classes[$i]->id;
         $name = $classes[$i]->name;
@@ -322,7 +397,9 @@ function renderClasses()
                 <td>$mutedUsers</td>
                 <td>$assignments</td>
                 <script>
+                    // Changes the delete button to a confirm button.
                     const delete_$id = () => document.getElementById("$id-delete").innerHTML = `<button onclick="deleteConfirm_$id();" class="btn btn-sm btn-danger">Confirm</button>`;
+                    // Runs an AJAX request to delete the class.
                     const deleteConfirm_$id = () => {
                         const classID = "$id";
                         $.post("Utils/Forms/deleteClass.php", { classID }, () => {
@@ -331,6 +408,7 @@ function renderClasses()
                         });
                     };
                 </script>
+                <!-- Delete button. -->
                 <td id="$id-delete">
                     <button onclick="delete_$id();" class="btn btn-sm btn-danger">Delete</button>
                 </td>
@@ -343,11 +421,14 @@ function renderClasses()
     return $html;
 }
 
+// Check which kind of user it is.
 switch ($user->type) {
     case "admin":
+        // Render the heading.
         echo <<<HTML
             <h1>Microcontroller Animations</h1>
         HTML;
+        // Call each render function from above.
         echo renderUsers();
         echo renderAnimations();
         echo renderPosts();
@@ -356,16 +437,23 @@ switch ($user->type) {
         break;
     case "teacher":
     case "student":
+        // Teachers and students get the same site.
+        // Get an array containing each post to show in the feed.
         $feedPosts = [];
         $feedUsers = $user->following;
         array_push($feedUsers, $user);
+        // Count each user so this doesn't need to be recalculated on each iteration.
         $feedUserCount = count($feedUsers);
+        // Loop through each user.
         for ($i = 0; $i < $feedUserCount; ++$i) {
             $thisUser = $feedUsers[$i];
             $thisPosts = $thisUser->posts;
+            // Count each post so this
             $thisPostCount = count($thisPosts);
+            // Add each post to the array.
             for ($j = 0; $j < $thisPostCount; ++$j) array_push($feedPosts, $thisPosts[$j]);
         }
+        // Sort the post by when they were created.
         usort($feedPosts, "sortByCreatedAt");
         echo <<<HTML
             <div style="display: flex; height: 100%;">
@@ -373,6 +461,7 @@ switch ($user->type) {
                     <div class="card-group" style="display: block; overflow: auto; height: 100%;">
                         <style>.post { display: none; }</style>
         HTML;
+        // Render each post.
         for ($i = 0; $i < min(count($feedPosts), 100); ++$i) echo $feedPosts[$i]->render() . "<br>";
         echo <<<HTML
                         <style>.post { display: block; }</style>
@@ -380,15 +469,17 @@ switch ($user->type) {
                 </div>
                 <div style="flex: 3%; max-width: 3%;"></div>
         HTML;
-
+        // Get all of the user's animations.
         $allAnimations = is_null($user->animations) ? [] : $user->animations;
         $animations = "";
+        // Render the first 3.
         for ($i = 0; $i < min(3, count($allAnimations)); ++$i) {
             $html = $allAnimations[$i]->render(false);
             $animations .= <<<HTML
                 <div class="col">$html</div>
             HTML;
         }
+        // If there are less than 3 animations, render a button to create a new one.
         if (count($allAnimations) < 3) $animations .= <<<HTML
             <div class="col">
                 <div class="card bg-dark text-light" style="height: 100%; min-height: 150px">
@@ -400,15 +491,17 @@ switch ($user->type) {
                 </div>
             </div>
         HTML;
-
+        // Get all of the user's classes.
         $allClasses = is_null($user->classes) ? [] : $user->classes;
         $classes = "";
+        // Render the first 3.
         for ($i = 0; $i < min(3, count($allClasses)); ++$i) {
             $html = $allClasses[$i]->render();
             $classes .= <<<HTML
                 <div class="col">$html</div>
             HTML;
         }
+        // If there are less than 3 classes, render a button to create/join a new one.
         if (count($allClasses) < 3) $classes .= <<<HTML
             <div class="col">
                 <div class="card bg-dark text-light" style="height: 100%; min-height: 100px">
@@ -420,7 +513,7 @@ switch ($user->type) {
                 </div>
             </div>
         HTML;
-
+        // Render the user's info.
         echo <<<HTML
                 <div style="flex: 57%; max-width: 57%; word-wrap: break-word;">
                     <h1>Microcontroller Animations</h1>
